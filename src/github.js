fc74778538e115d70caf2d81654b7f14ef23058f
John Hannagan <weetbix_@hotmail.com>
2020-12-16 16:35:21 +0100

    WIP2

---
+++ src/github.js
@@ -0,0 +1,83 @@
+// Not needed for now, but could be useful
+// const createStatus = async ({ client, context, sha, status }) =>
+// 	client.repos.createCommitStatus({
+// 		...context.repo,
+// 		sha,
+// 		...status,
+// 	})
+
+const hiddenHeader = `<!-- monorepo-jest-reporter-action -->`;
+
+const appendHiddenHeaderToComment = (body) =>
+	hiddenHeader + body
+
+const listComments = async ({ client, context, prNumber, commentHeader }) => {
+	const { data: existingComments } = await client.issues.listComments({
+		...context.repo,
+		issue_number: prNumber,
+	})
+
+	return existingComments.filter(({ body }) => body.startsWith(hiddenHeader))
+}
+
+const insertComment = async ({ client, context, prNumber, body }) =>
+	client.issues.createComment({
+		...context.repo,
+		issue_number: prNumber,
+		appendHiddenHeaderToComment(body),
+	})
+
+const updateComment = async ({ client, context, body, commentId }) =>
+	client.issues.updateComment({
+		...context.repo,
+		comment_id: commentId,
+		appendHiddenHeaderToComment(body),
+	})
+
+const deleteComments = async ({ client, context, comments }) =>
+	Promise.all(
+		comments.map(({ id }) =>
+			client.issues.deleteComment({
+				...context.repo,
+				comment_id: id,
+			}),
+		),
+	)
+
+const upsertComment = async ({
+	client,
+	context,
+	prNumber,
+	body,
+}) => {
+    const existingComments = await listComments({
+        client,
+        context,
+        prNumber
+      });
+	const last = existingComments.pop();
+
+	await deleteComments({
+		client,
+		context,
+		comments: existingComments,
+	})
+
+	return last
+		? updateComment({
+				client,
+				context,
+				body,
+				commentId: last.id,
+		  })
+		: insertComment({
+				client,
+				context,
+				prNumber,
+				body,
+		  })
+}
+
+module.exports = {
+    upsertComment
+}
